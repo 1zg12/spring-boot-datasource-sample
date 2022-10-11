6ca7823f991bed742f2c90b5c8bc941879523fba
j1zg <lwpro2@live.com>
2022-10-11 15:25:50 +0800

    sample project to load spring-boot-datasource

---
+++ src/main/java/org/example/controller/Sample.java
@@ -0,0 +1,13 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Sample {
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "sample";
+    }
+}
